e31d2a8a63002f10196fa24e7bef362a4f096351
Susmitha_Pathi <42517449+sweetyRamesh@users.noreply.github.com>
2023-02-01 14:57:10 -0500

    Update Largest.java

--- Largest.java
+++ Largest.java
@@ -1,17 +1,20 @@
 //without java 8 
 public class Largest {
-  public static void main(String[] args) {
-    int[] arr = {10, 20, 25, 40, 50, 60};
-    int max1 = Integer.MIN_VALUE;
-    int max2 = Integer.MIN_VALUE;
-    for (int n : arr) {
-      if (n > max1) {
-        max2 = max1;
-        max1 = n;
-      } else if (n > max2 && n != max1) {
-        max2 = n;
+   public static void main(String args[]){
+      int temp, size;
+      int array[] = {10, 20, 25, 63, 96, 57};
+      size = array.length;
+
+      for(int i = 0; i<size; i++ ){
+         for(int j = i+1; j<size; j++){
+
+            if(array[i]>array[j]){
+               temp = array[i];
+               array[i] = array[j];
+               array[j] = temp;
+            }
+         }
       }
-    }
-    System.out.println("Second largest number is: " + max2);
-  }
+      System.out.println("second largest number is:: "+array[size-2]);
+   }
 }
